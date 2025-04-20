94bec50ebc89845f118fd4723de93c3f0de526ce
TotoMessina <132374777+TotoMessina@users.noreply.github.com>
2025-04-20 13:00:00 -0300

    1.0.3

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    // Aquí deberás colocar la URL de tu script de Google Apps una vez que lo publiques
-    API_URL: 'https://script.google.com/macros/s/AKfycbxYSdtam3kihTDegxrAYqiDNFIi-nF2gwN2RH7pFRKr_R45u7rmUZ1I7HsGbmErysZ9/exec'
-}; 
