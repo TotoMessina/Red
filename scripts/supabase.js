bc96a70ab107020f45ca43317ae9cf206da92372
TotoMessina <132374777+TotoMessina@users.noreply.github.com>
2025-04-21 22:41:59 -0300

    1.0.7

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,3 +1,3 @@
-const supabaseUrl = 'https://uqnmrfgmskvbfjsvhmzo.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVxbm1yZmdtc2t2YmZqc3ZobXpvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyMDQ3MDEsImV4cCI6MjA2MDc4MDcwMX0.dfLwm5gVLN-iPqn2pmW2ZylNyrZezttD7ShEX6JVIRY';
+const supabaseUrl = 'https://qnmrkwjshfktwlbketvw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubXJrd2pzaGZrdHdsYmtldHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyODUzNjAsImV4cCI6MjA2MDg2MTM2MH0.AZrGvp5FE1_B-D8oDbVgLao0cBnBW70lFqFVKlVs6Wg';
 const supabase = supabase.createClient(supabaseUrl, supabaseKey);
