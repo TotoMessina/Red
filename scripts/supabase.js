1816ec2d108f5322de1242c5c5fc3fea55c6a8a6
Lucas Messina <132374777+TotoMessina@users.noreply.github.com>
2025-06-07 00:18:01 -0300

    Delete scripts directory

--- scripts/supabase.js
+++
@@ -1,3 +0,0 @@
-const supabaseUrl = 'https://qnmrkwjshfktwlbketvw.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubXJrd2pzaGZrdHdsYmtldHZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUyODUzNjAsImV4cCI6MjA2MDg2MTM2MH0.AZrGvp5FE1_B-D8oDbVgLao0cBnBW70lFqFVKlVs6Wg';
-const supabase = supabase.createClient(supabaseUrl, supabaseKey);
